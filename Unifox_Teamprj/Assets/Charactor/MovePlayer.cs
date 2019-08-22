c1ba3fc491e45b6acdeed0acbdf031b265dd13fc
Pican <dlxogus0619@naver.com>
2019-08-22 10:29:59 +0900

    Unity_base
    FirstCommit

---
+++ Unifox_Teamprj/Assets/Charactor/MovePlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public float speed;
+
+    private Vector3 vector;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            while (currentWalkCount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * speed, 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * speed, 0);
+                }
+                currentWalkCount++;
+
+            }
+            currentWalkCount = 0;
+            yield return new WaitForSeconds(0.1f);
+
+        }
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
+        }
+        
+    }
+}
